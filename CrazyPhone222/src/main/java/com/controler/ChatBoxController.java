19d12377a8394446075086b331b0036a14e7743c
student <student@DESKTOP-KFPK72R>
2020-07-23 16:26:30 +0800

    自動聊天 健哲 0723

--- CrazyPhone222/src/main/java/com/controler/ChatBoxController.java
+++ CrazyPhone222/src/main/java/com/controler/ChatBoxController.java
@@ -1,6 +1,9 @@
 package com.controler;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
--- CrazyPhone222/src/main/java/com/websocket/MyWebSocketHandler.java
+++ CrazyPhone222/src/main/java/com/websocket/MyWebSocketHandler.java
@@ -3,15 +3,23 @@ package com.websocket;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+import javax.servlet.http.HttpSession;
+
+import org.omg.CORBA.WStringSeqHelper;
 import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import com.google.errorprone.annotations.Var;
+
 @Component
 public class MyWebSocketHandler extends TextWebSocketHandler {
-
+	public static final String UTF8_BOM = "\uFEFF"; // 定義 UTF-8的BOM字元
+	static boolean flag = true;
 	// 用来保存连接进来session
 	private List<WebSocketSession> sessions = new CopyOnWriteArrayList<>();
 
@@ -35,12 +43,25 @@ public class MyWebSocketHandler extends TextWebSocketHandler {
 			ws.sendMessage(new TextMessage(clientMessage));
 		}
 
-//      if (clientMessage.startsWith("Hello") || clientMessage.startsWith("Hi")) {
-//         session.sendMessage(new TextMessage("Hello! What can i do for you?"));
-//      } else {
-//         session.sendMessage(
-//               new TextMessage("This is a simple hello world example of using Spring WebSocket."));
-//      }
-//   }
+		if (clientMessage.startsWith(UTF8_BOM)) {
+			clientMessage = clientMessage.substring(1);
+		}
+		String[] tokens = clientMessage.split(":");
+		
+	
+		if (tokens[0] != "LISA" && tokens[1].equals("hi")) {
+			session.sendMessage(new TextMessage("How can I help you?"));
+		} else if (tokens[0] != "LISA" && tokens[1].contains("爛")) {
+			session.sendMessage(new TextMessage("We apologize for any inconvenience caused."));
+		} else if (tokens[0].equals("LISA") && flag == true) {
+			
+			flag=false;
+		}
+		else if (tokens[0]!="LISA" && flag == true) {
+			
+			session.sendMessage(new TextMessage("服務人員忙線中,預計30分鐘內回覆您的問題!"));
+			flag=false;
+		}
+
 	}
 }
